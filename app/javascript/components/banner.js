b31579bd1ed1d969102293123a2e9ad28dd27f56
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-03-26 17:54:37 +0200

    home page in progress

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ["Un bon verre.", "Une bonne table.", "Du bon son."],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
