9ca93688fdd23c76a9e6a066d2b8f76892826011
Pierre Girbau <pierregirbau@gmail.com>
2018-03-29 14:34:20 +0200

    Centered flatpickr calendar and add placeholder

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,4 +2,7 @@ import flatpickr from "flatpickr"
 
 import "flatpickr/dist/themes/dark.css" // A path to the theme CSS
 
-flatpickr(".datepicker", {})
+flatpickr(".datepicker", {
+  altInput: true,
+  allowInput: true
+})
