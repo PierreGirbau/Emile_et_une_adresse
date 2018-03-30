7eab6d1119221c9585b84f0d2d098830a91fa673
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-03-30 12:52:26 +0200

    need to do the trash for destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  root to: "places#index"
+
   resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
