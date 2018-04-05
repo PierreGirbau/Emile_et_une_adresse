db437af09da2a0588d032e4c8cb267f34b683e45
Pierre Girbau <pierregirbau@gmail.com>
2018-04-05 15:24:31 +0200

    Switched from flatpickr to input date

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,12 +1,9 @@
 import flatpickr from "flatpickr"
-
 import "flatpickr/dist/themes/dark.css" // A path to the theme CSS
-
 function flatpickrr() {
   flatpickr(".datepicker", {
     altInput: true,
     allowInput: true
   });
 };
-
 export { flatpickrr };
