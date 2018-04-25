03e15392dc1058a61825bd8dd30a9539abad9ca3
Pierre Girbau <pierregirbau@gmail.com>
2018-04-25 09:58:14 +0200

    Fixed Flatpickr date.today

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,7 +3,7 @@ import "flatpickr/dist/themes/dark.css" // A path to the theme CSS
 function flatpickrr() {
   flatpickr(".datepicker", {
     altInput: true,
-    allowInput: true
+    allowInput: true,
   });
 };
 export { flatpickrr };
